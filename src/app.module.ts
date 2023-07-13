dc772e43e14723469641416cf412be9ea9b59745
Rodrigo Marcondes <rodrigomarcondes2000@gmail.com>
2023-07-13 10:56:12 -0300

    Removing existing app controllers and service

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,9 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { CreaturesModule } from './creatures/creatures.module';
 
 @Module({
   imports: [CreaturesModule],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
--- src/creatures/creatures.module.ts
+++ src/creatures/creatures.module.ts
@@ -2,8 +2,9 @@ import { Module } from '@nestjs/common';
 import { CreaturesController } from './creatures.controller';
 import { CreaturesService } from './creatures.service';
 
-@Module({})
-export class CreaturesModule {
-  controllers: [CreaturesController]
-  providers: [CreaturesService]
-}
+@Module({
+  controllers: [CreaturesController],
+  providers: [CreaturesService],
+  exports: [CreaturesService],
+})
+export class CreaturesModule {}
