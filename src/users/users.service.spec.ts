d3add044c7c900ef5c6cb1dbd3d7a4ea0d357144
Rodrigo Marcondes <rodrigomarcondes2000@gmail.com>
2023-07-15 03:17:16 -0300

    Improving user service and controller unit test

--- src/users/users.service.spec.ts
+++ src/users/users.service.spec.ts
@@ -1,18 +1,63 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UsersService } from './users.service';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { User } from './user.entity';
+import { Repository } from 'typeorm';
 
 describe('UsersService', () => {
   let service: UsersService;
+  let userRepository: Repository<User>;
+
+  const USER_REPOSITORY_TOKEN = getRepositoryToken(User);
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [UsersService],
+      providers: [
+        UsersService,
+        {
+          provide: USER_REPOSITORY_TOKEN,
+          useValue: {
+            create: jest.fn(),
+            save: jest.fn(),
+            find: jest.fn(),
+            findOneBy: jest.fn(),
+            delete: jest.fn(),
+          },
+        },
+      ],
     }).compile();
 
     service = module.get<UsersService>(UsersService);
+    userRepository = module.get<Repository<User>>(USER_REPOSITORY_TOKEN);
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  it('userRepository should be defined', () => {
+    expect(userRepository).toBeDefined();
+  });
+
+  describe('create', () => {
+    it("should save a user with it's password hashed using the user repository", async () => {
+      const mockedPasswordHash = 'password_hashed';
+      jest
+        .spyOn(service as any, 'hashPassword')
+        .mockResolvedValue(mockedPasswordHash);
+
+      const createUserDto = {
+        name: 'John Doe',
+        email: 'email@example.com',
+        password: 'password',
+      };
+      await service.create(createUserDto);
+
+      expect(userRepository.save).toHaveBeenCalledWith({
+        name: createUserDto.name,
+        email: createUserDto.email,
+        password_hash: mockedPasswordHash,
+      });
+    });
   });
 });
