ba163ee52b9d504072e61a308f4a568e761abea1
Rodrigo Marcondes <rodrigomarcondes2000@gmail.com>
2023-09-16 19:29:20 -0300

    Creating User Repository implementationm for TypeORM

---
+++ src/common/repositories/typeorm/user-typeorm.repository.ts
@@ -0,0 +1,11 @@
+import { Repository } from 'typeorm';
+import { IUserRepository } from '../../../@core/domain/user/user.repository';
+import { User } from '../../../@core/domain/user/user.entity';
+
+export class UserTypeOrmRepository implements IUserRepository {
+  constructor(private ormRepository: Repository<User>) {}
+
+  async insert(user: User): Promise<void> {
+    await this.ormRepository.save(user);
+  }
+}
